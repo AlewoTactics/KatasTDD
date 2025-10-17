d05d1e4206d447b537d6da61e57c31249af6e7f2
Alejandro Melo <daniel-alejandro_135@hotmail.com>
2025-10-17 11:02:25 -0500

    Punto inicial dsde trabajo en clase marsRover

---
+++ Domain/MarsRover.cs
@@ -0,0 +1,41 @@
+﻿namespace Domain;
+
+public class MarsRover
+{
+    private int PosicionX { get; set; } = 0;
+    private int PosicionY { get; set; } = 0;
+    private int PuntoCardinal { get; set; } = 0;
+    
+
+    private readonly Dictionary<int, char> _puntosCardinales = new ()
+    {
+        { 0, 'N' },
+        { 1, 'E' }, 
+        { 2, 'S' },
+        { 3, 'W' }
+    };
+    public string ObtenerUbicacion() =>  $"{PosicionX}:{PosicionY}:{_puntosCardinales[PuntoCardinal]}";
+    
+    public void RealizarMovimientos(string movimiento)
+    {
+        foreach (var comando in movimiento)
+        {
+            if (HayUnGiroALaDerecha(comando))
+                GirarDerecha();
+            else
+                MoverY();
+        }
+    }
+
+    private void MoverY()
+    {
+        const int limitePlataforma = 10;
+        if (PosicionY >= limitePlataforma)
+            PosicionY = 0;
+        PosicionY++;
+    }
+
+    private void  GirarDerecha() => PuntoCardinal = (PuntoCardinal + 1) % 4;
+
+    private bool HayUnGiroALaDerecha(char movimientos) => movimientos.Equals('R');
+}
---
+++ Test/MarsRoverTest.cs
@@ -0,0 +1,80 @@
+﻿using Domain;
+using FluentAssertions;
+
+namespace Test;
+
+public class MarsRoverTest
+{
+    [Theory]
+    [InlineData("", "0:0:N")]
+    [InlineData("M", "0:1:N")]
+    [InlineData("MM", "0:2:N")]
+    [InlineData("MMM", "0:3:N")]
+    [InlineData("MMMMMMMMMMMMM", "0:3:N")]
+    public void Si_Ingreso_Movimientos_Desde_El_Origen_Mirando_Al_norte_debe_retornarLaCoordenada(string movimiento,
+        string coordenadaEsperada)
+    {
+        //arrange
+        var marsRovers = new MarsRover();
+        //act
+        marsRovers.RealizarMovimientos(movimiento);
+        //assert
+        marsRovers.ObtenerUbicacion().Should().Be(coordenadaEsperada);
+    }
+
+    [Fact]
+    public void Si_Ingreso_Derecha_Mirando_Al_Norte_Debe_Retornar_La_Coordenada_Cero_Cero_Este()
+    {
+        //arrange
+        var marsRovers = new MarsRover();
+        //act
+        marsRovers.RealizarMovimientos("R");
+        //assert
+        marsRovers.ObtenerUbicacion().Should().Be("0:0:E");
+    }
+
+    [Fact]
+    public void Si_Ingreso_Dos_Giros_A_La_Derecha_Debe_Retornar_La_Coordenada_Cero_Cero_Sur()
+    {
+        //arrange
+        var marsRovers = new MarsRover();
+        //act
+        marsRovers.RealizarMovimientos("RR");
+        //assert
+        marsRovers.ObtenerUbicacion().Should().Be("0:0:S");
+    }
+    
+    [Fact]
+    public void Si_Ingreso_Tres_Giros_A_La_Derecha_Debe_Retornar_La_Coordenada_Cero_Cero_Oeste()
+    {
+        //arrange
+        var marsRovers = new MarsRover();
+        
+        //act
+        marsRovers.RealizarMovimientos("RRR");
+        //assert
+        marsRovers.ObtenerUbicacion().Should().Be("0:0:W");
+    }
+    
+    [Fact]
+    public void Si_Ingreso_Cinco_Giros_A_La_Derecha_Debe_Retornar_La_Coordenada_Cero_Cero_Este()
+    {
+        //arrange
+        var marsRovers = new MarsRover();
+        //act
+        marsRovers.RealizarMovimientos("RRRRR");
+        //assert
+        marsRovers.ObtenerUbicacion().Should().Be("0:0:E");
+    }
+    
+    [Fact]
+    public void Si_Ingreso_UnMovimiento_y_un_giro_debe_retornar_Cero_Uno_Este()
+    {
+        //arrange
+        var marsRovers = new MarsRover();
+        //act
+        marsRovers.RealizarMovimientos("MR");
+        //assert
+        marsRovers.ObtenerUbicacion().Should().Be("0:1:E");
+    }
+}
