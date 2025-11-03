d4b784beb07b4f1ec65daa39d4c7cb68937da231
AlewoTactics <daniel-alejandro_135@hotmail.com>
2025-11-03 16:01:58 -0500

    🔴SI_SonEntre6Y7AM_Debe_RetornarHacerEjercicio

---
+++ Test/RutinasMatutinasTest.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace Test;
+
+public class RutinasMatutinasTest
+{
+    [Fact]
+    public void SI_SonEntre6Y7AM_Debe_RetornarHacerEjercicio()
+    {
+        var tarea = QueDeboHacerAhora();
+        tarea.Should().Be("Hacer ejercicio");
+    }
+
+    private string QueDeboHacerAhora()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb68bdcd87c412ba85b4af4b19b7dae3b824315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530d7384e6dff8cc5b5af50c0777279ec6180398")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
