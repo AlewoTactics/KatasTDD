9f4b02093eab663c20f65a62a3beba2447635080
AlewoTactics <daniel-alejandro_135@hotmail.com>
2025-11-03 17:48:58 -0500

    ✅SI_SonEntre8Y8H59mAM_Debe_RetornarDesayunar

--- Domain/PersonalTaskManager.cs
+++ Domain/PersonalTaskManager.cs
@@ -16,11 +16,18 @@ public class PersonalTaskManager
         
         TimeSpan horaInicio7 = new TimeSpan(7, 0, 0); 
         TimeSpan horaFin7 = new TimeSpan(7, 59, 0);
+        
+        TimeSpan horaInicio8 = new TimeSpan(8, 0, 0); 
+        TimeSpan horaFin8 = new TimeSpan(8, 59, 0);
+        
         if (_horaActual >= horaInicio6 && _horaActual <= horaFin6)
             return "Hacer ejercicio"; 
         
         if (_horaActual >= horaInicio7 && _horaActual <= horaFin7)
             return "Leer y estudiar";
+        
+        if (_horaActual >= horaInicio8 && _horaActual <= horaFin8)
+            return "Desayunar";
         
         return "";
     }
