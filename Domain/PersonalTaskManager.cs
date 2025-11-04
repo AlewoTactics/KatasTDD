abe8fac9e0c0d8b4896f812fc6e120f584cdab79
AlewoTactics <daniel-alejandro_135@hotmail.com>
2025-11-03 19:28:22 -0500

    🔷Refactor test y etandarizacion de tareas y asignacion de horaActual

--- Domain/PersonalTaskManager.cs
+++ Domain/PersonalTaskManager.cs
@@ -1,30 +1,24 @@
 ﻿namespace Domain;
 
-
-
 public class PersonalTaskManager
 {
     private TimeSpan _horaActual;
+    private List<Tarea> _tareas;
 
-    public PersonalTaskManager(TimeSpan horaActual)
+    public PersonalTaskManager(List<Tarea> tareas)
     {
-        _horaActual = horaActual;
+        _tareas = tareas;
     }
+
+    public void AsignarHoraActual(TimeSpan horaACtual) => _horaActual = horaACtual;
     
     public string QueDeboHacerAhora()
     {
-        List<Tarea> tareas = [
-            new Tarea("Hacer ejercicio", new TimeSpan(6, 0, 0), new TimeSpan(6, 59, 0)),
-            new Tarea("Leer y estudiar", new TimeSpan(7, 0, 0), new TimeSpan(7, 59, 0)),
-            new Tarea("Desayunar", new TimeSpan(8, 0, 0), new TimeSpan(8, 59, 0))
-        ];
-
-        foreach (Tarea tarea in tareas)
+        foreach (Tarea tarea in _tareas)
         {
             if (_horaActual >= tarea.horaInicio && _horaActual <= tarea.horaFin)
                 return tarea.nombre;
         }
-        
         return "Sin actividad";
     }
 }
--- Test/RutinasMatutinasTest.cs
+++ Test/RutinasMatutinasTest.cs
@@ -5,13 +5,25 @@ namespace Test;
 
 public class RutinasMatutinasTest
 {
-
+    private List<Tarea> _tareas;
+    private PersonalTaskManager  _personalTaskManager;
+    public RutinasMatutinasTest()
+    {
+        _tareas = [
+            new Tarea("Hacer ejercicio", new TimeSpan(6, 0, 0), new TimeSpan(6, 59, 0)),
+            new Tarea("Leer y estudiar", new TimeSpan(7, 0, 0), new TimeSpan(7, 59, 0)),
+            new Tarea("Desayunar", new TimeSpan(8, 0, 0), new TimeSpan(8, 59, 0))
+        ];
+        _personalTaskManager = new PersonalTaskManager(_tareas);     
+    }
+    
+    
     [Fact]
     public void SI_SonEntre6Y6H59mAM_Debe_RetornarHacerEjercicio()
     {
         TimeSpan horaActual = new TimeSpan(6, 0, 0);
-        var personalTaskManager = new PersonalTaskManager(horaActual);  
-        var tarea = personalTaskManager.QueDeboHacerAhora();
+        _personalTaskManager.AsignarHoraActual(horaActual);
+        var tarea = _personalTaskManager.QueDeboHacerAhora();
         tarea.Should().Be("Hacer ejercicio");
     }
     
@@ -20,24 +32,24 @@ public class RutinasMatutinasTest
     public void SI_SonEntre7Y7H59mAM_Debe_RetornarLeeryestudiar()
     {
         TimeSpan horaActual = new TimeSpan(7, 0, 0);
-        var personalTaskManager = new PersonalTaskManager(horaActual);
-        personalTaskManager.QueDeboHacerAhora().Should().Be("Leer y estudiar");
+        _personalTaskManager.AsignarHoraActual(horaActual);
+        _personalTaskManager.QueDeboHacerAhora().Should().Be("Leer y estudiar");
     }
     
     [Fact]
     public void SI_SonEntre8Y8H59mAM_Debe_RetornarDesayunar()
     {
         TimeSpan horaActual = new TimeSpan(8, 0, 0);
-        var personalTaskManager = new PersonalTaskManager(horaActual);
-        personalTaskManager.QueDeboHacerAhora().Should().Be("Desayunar");
+        _personalTaskManager.AsignarHoraActual(horaActual);
+        _personalTaskManager.QueDeboHacerAhora().Should().Be("Desayunar");
     }
 
     [Fact]
     public void SI_NoHayTareasASignadasEnHorario_debe_RetornarSinActividad()
     {
         TimeSpan horaActual = new TimeSpan(9, 0, 0);
-        var personalTaskManager = new PersonalTaskManager(horaActual);
-        personalTaskManager.QueDeboHacerAhora().Should().Be("Sin actividad");
+        _personalTaskManager.AsignarHoraActual(horaActual);
+        _personalTaskManager.QueDeboHacerAhora().Should().Be("Sin actividad");
     }
 
     
