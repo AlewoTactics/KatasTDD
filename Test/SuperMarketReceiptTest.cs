0d1f80651012230911feddf48bfebe733b236f94
AlewoTactics <daniel-alejandro_135@hotmail.com>
2025-11-10 17:08:37 -0500

    ✅SI_Agregoarrozcarrito_Debe_Serelvalortotal2_49

--- Test/SuperMarketReceiptTest.cs
+++ Test/SuperMarketReceiptTest.cs
@@ -9,7 +9,7 @@ public class SuperMarketReceiptTest
     {
         var carrito = new Carrito();
         carrito.Agregar(new PastaDeDientes());
-        carrito.ObtenerValorTotal().Should().Be((decimal)0.99);
+        carrito.ObtenerValorTotal().Should().Be(0.99m);
     }
     
     [Fact]
@@ -17,7 +17,15 @@ public class SuperMarketReceiptTest
     {
         var carrito = new Carrito();
         carrito.Agregar(new Manzana());
-        carrito.ObtenerValorTotal().Should().Be((decimal)1.99);
+        carrito.ObtenerValorTotal().Should().Be(1.99m);
+    }
+    
+    [Fact]
+    public void SI_Agregoarrozcarrito_Debe_Serelvalortotal2_49()
+    {
+        var carrito = new Carrito();
+        carrito.Agregar(new Arroz());
+        carrito.ObtenerValorTotal().Should().Be(2.49m);
     }
 }
 
@@ -25,19 +33,40 @@ public class SuperMarketReceiptTest
 
 public class Carrito
 {
-    public void Agregar(PastaDeDientes pastaDeDientes)
+    private Articulo _articulo;
+
+
+    public void Agregar(Articulo articulo)
     {
-        
+        _articulo = articulo;
     }
 
-    public decimal ObtenerValorTotal() => (decimal)0.99;
+    public decimal ObtenerValorTotal()
+    {
+        if (_articulo.GetType() == typeof(Manzana))
+            return 1.99m;
+        else if (_articulo.GetType() == typeof(Arroz))
+            return 2.49m;
+        else
+        {
+            return 0.99m;
+        }
+    }
 }
 
-public class PastaDeDientes 
+public class PastaDeDientes: Articulo
 {
   
 }
-public class Manzana : PastaDeDientes
+public class Manzana : Articulo
+{
+}
+
+public class Arroz : Articulo
+{
+}
+
+public class Articulo
 {
 }
 
