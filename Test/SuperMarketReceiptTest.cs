12478194d7c44e5b9aa57e9b44f68f1ab722c5df
AlewoTactics <daniel-alejandro_135@hotmail.com>
2025-11-10 16:45:09 -0500

    🔴SI_Agregounapastadedientesalcarrito_Debe_Serelvalortotal099

---
+++ Test/SuperMarketReceiptTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Test;
+
+public class SuperMarketReceiptTest
+{
+    [Fact]
+    public void SI_Agregounapastadedientesalcarrito_Debe_Serelvalortotal099()
+    {
+        var carrito = new Carrito();
+        carrito.Agregar(new PastaDeDientes());
+        carrito.ObtenerValorTotal();
+    }
+}
+
+public class Carrito
+{
+    public void Agregar(PastaDeDientes pastaDeDientes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal ObtenerValorTotal()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class PastaDeDientes 
+{
+  
+}
+
